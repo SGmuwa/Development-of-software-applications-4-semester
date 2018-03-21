9981080952edb4ae77ac539c03b495fd54cd3175
SGmuwa <motherlode.muwa@gmail.com>
2018-03-21 20:18:37 +0300

    Уточнена структура
    Код теста был пройдён разработчиком второй раз. Уточнён характер системы. К каждой строке программы был написан комментарий.

--- Лаба 2/Test_Weather.java
+++ Лаба 2/Test_Weather.java
@@ -11,45 +11,62 @@
 	}
 	public static void main(String[] args)
 	{
-		log(true, "Test started");
-		TaskGenerator tasks = new TaskGenerator(100);
-		if (tasks.length != 100) log(false, "TaskGenerator: length = " + tasks.length);
-		else log(true, "TaskGenerator: length.");
-		if(isTasksRandomize(tasks)) log(true, "TaskGenerator: IsRandomize");
-		else log(false, "TaskGenerator: Not randomize");
+		log(true, "Test started"); // Начало тестирования
 		
-		WeatherConveyer wc = new WeatherConveyer();
-		while(!tasks.isNeedGenerate) wc.quiueInbox.add(tasks.getTask());
-		if(wc.quiueInbox != null) log(true, "quiueInbox is not null.");
+		int countElms = 100; // количество тестируемых элементов
+		
+		TaskGenerator tasks = new TaskGenerator(countElms); // Готовим генератор задач к работе.
+		if (tasks.getCount() != countElms) // В случае, если значение не верно.
+			log(false, "TaskGenerator: count = " + tasks.getCount()); // Возвращаем ошибку.
+		else log(true, "TaskGenerator: xcount."); // Иначе отвечаем, что всё хорошо.
+		if(isTasksRandomize(tasks)) log(true, "TaskGenerator: IsRandomize"); // Проверяем, что задачи наши не одни и те же.
+		else log(false, "TaskGenerator: Not randomize"); // В случае, если есть схожести, пишем об этом.
+		
+		WeatherConveyer wc = new WeatherConveyer(); // Готовим основной конвеер к работе.
+		
+		if(wc.quiueInbox != null)
+			log(true, "quiueInbox is not null."); // Входящая очередь должна при этом инцелизироваться.
 		else
-		{
+		{ // Если нет, то тест нельзя дальше производить.
 			log(false, "quiueInbox is null.");
 			return;
 		}
-		if(wc.quiueInbox.count == tasks.length) log(true, "wc.quiueInbox: count == tasks.length == " + tasks.length);
-		else log(false, "wc.quiueInbox: count (" + wc.quiueInbox.count + ") != tasks.length (" + tasks.length + ")");
+		
+		while(!tasks.isNeedGenerate()) // Пока он не нужно генерировать.
+			wc.quiueInbox.add(tasks.getTask()); // Добавить в входящую очередь задачу.
+			
+		if(wc.quiueInbox.getCount() == countElms) // В входной очереди должно быть countElms элементов.
+			log(true, "wc.quiueInbox: count == countElms == " + countElms);
+		else log(false, "wc.quiueInbox: count (" + wc.quiueInbox.getCount + ") != countElms (" + countElms + ")");
 
-		if(wc.quiueOutbox != null) log(true, "quiueOutbox is not null.")
+		if(wc.quiueOutbox != null) // Выходная очередь должна быть инцилизирована.
+			log(true, "quiueOutbox is not null.")
 		else
 		{
 			log(false, "quiueOutbox is null.");
 			return;
 		}
 		
-		wc.stepGetWeatherFromInboxToOutbox();
+		wc.stepGetWeatherFromInboxToOutbox(); // Выполняем один шаг.
 
-		if(wc.quiueOutbox.count == 1) log(true, "quiueOutbox.count = 1");
-		else log(false, "quiueOutbox.count = " + wc.quiueOutbox.count);
+		if(wc.quiueOutbox.getCount() == 1) // Проверяем, что в выходной очереди действительно оказался 1 элемент.
+			log(true, "quiueOutbox.count = 1");
+		else
+			log(false, "quiueOutbox.count = " + wc.quiueOutbox.getCount());
 
-		if(wc.quiueInbox.count == 99) log(true, "quiueOutbox.count = 99");
-		else log(false, "quiueInbox.count = " + wc.quiueOutbox.count);
+		if(wc.quiueInbox.getCount == countElms - 1)
+			log(true, "quiueOutbox.count = 99"); // Проверяем, что из входной один элемент пропал.
+		else
+			log(false, "quiueInbox.count = " + wc.quiueOutbox.count);
 
-		wc.start();
+		wc.startGetWeatherFromInboxToOutbox(); // Начать высчитывать в ассинхронном режиме.
 		
 		Thread.sleep(100); // wait 100ms
 
-		if(wc.quiueOutbox.count == 100) log(true, "quiueOutbox.count = 100.");
-		else log(false, "quiueOutbox.count = " + wc.quiueOutbox.count);
+		if(wc.quiueOutbox.count == countElms) // Проверяем, что вся выходная очередь заполнена.
+			log(true, "quiueOutbox.count = " + wc.quiueOutbox.getCount);
+		else 
+			log(false, "quiueOutbox.count = " + wc.quiueOutbox.getCount);
 		
 	}
 
